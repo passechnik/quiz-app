0882e86636cfbb4eaf4a209c5f12ed5b13059e4f
Valerie Kucher <passechnik214@gmail.com>
2023-07-13 02:03:41 +0300

    add basic styles

--- script.js
+++ script.js
@@ -1,73 +1,78 @@
-let quiz = [
-    {
-        question: "Перше прізвисько автора квізу?",
-        options: ["Пупік", "Гунган", "Сплюн"],
-        answer: 1
-    },
-
-    {
-        question: "Перше прізвисько автора квізу?"
-        options: ["Пупік", "Гунган", "Сплюн"]
-        answer:
-    }, 
-    {
-        question: "Перше прізвисько автора квізу?",
-        options: ["Пупік", "Гунган", "Сплюн"],
-        answer: 1
-    },
-    {
-        question: "Перше прізвисько автора квізу?",
-        options: ["Пупік", "Гунган", "Сплюн"],
-        answer: 1
-    },
-
-]
-
-let currentQuestion = 0;
-ler score = 0;
-let quizContainer = document.getElementById('question');
-let optionsContainer = document.getElementById('options');
-let submitButton = document.getElementById('sumbit');
-let resultsContainer = document.getElementById('results');
-
-function displayQuestion() {
-    let question = quiz[currentQuestion].question;
-    let optaions = quiz[currentQuestion].options;
-
-    quizContainer.textContent = question;
-    optionsContainer.innerHTML = '';
-
-    for( let i = 0; i < options.length; i++) {
-        let option = document.createElement('button');
-        option.textContent = option[i];
-        option.value = 1;
-        option.className = 'option';
-        option.addEventListener('click',handleAnswer);
-        optionsContainer.appendChild(option)
+document.addEventListener('DOMContentLoaded', function() {
+    let quiz = [
+        {
+            question: "Перше прізвисько автора квізу?",
+            options: ["Пупік", "Гунган", "Сплюн"],
+            answer: 1
+        },
+    
+        {
+            question: "Перше прізвисько автора квізу?",
+            options: ["Пупік", "Гунган", "Сплюн"],
+            answer: 1
+        }, 
+        {
+            question: "Перше прізвисько автора квізу?",
+            options: ["Пупік", "Гунган", "Сплюн"],
+            answer: 1
+        },
+        {
+            question: "Перше прізвисько автора квізу?",
+            options: ["Пупік", "Гунган", "Сплюн"],
+            answer: 1
+        },
+    
+    ]
+    
+    let currentQuestion = 0;
+    let score = 0;
+    
+    
+    let quizContainer = document.getElementById('question');
+    let optionsContainer = document.getElementById('options');
+    let submitButton = document.getElementById('sumbit');
+    let resultsContainer = document.getElementById('results');
+    
+    function displayQuestion() {
+        let question = quiz[currentQuestion].question;
+        let options = quiz[currentQuestion].options;
+    
+        quizContainer.textContent = question;
+        optionsContainer.innerHTML = '';
+        resultsContainer.style.display = 'none'
+    
+        for( let i = 0; i < options.length; i++) {
+            let option = document.createElement('button');
+            option.textContent = options[i];
+            option.value = 1;
+            option.className = 'option';
+            option.addEventListener('click',handleAnswer);
+            optionsContainer.appendChild(option)
+        }
     }
-}
-
-function handleAnswer(e) {
-    let selectedOption = parseInt(e.target.value);
-
-    if (selectedOption === quiz[currentQuestion].answer) {
-        score ++;
+    
+    function handleAnswer(e) {
+        let selectedOption = parseInt(e.target.value);
+    
+        if (selectedOption === quiz[currentQuestion].answer) {
+            score ++;
+        }
+    
+        currentQuestion++;
+    
+        if(currentQuestion < quiz.length) {
+            displayQuestions();
+        } else {
+            displayResults();
+        }
     }
-
-    currentQuestion++;
-
-    if(currentQuestion < quiz.length) {
-        displayQuestions();
-    } else {
-        displayResults();
+    
+    function displayResults() {
+        quizContainer.style.display = 'none';
+        optionsContainer.style.display = 'none';
+        submitButton.style.display = 'none';
+        resultsContainer.textContent = "Твій результат";
     }
-}
-
-function displayResults() {
-    quizContainer.style.display = 'none';
-    optionsContainer.style.display = 'none';
-    submitButton.style.display = 'none';
-    resultsContainer.textContent = 'Твій результат' + score + 'з' + quiz.length
-}
-
-displayQuestion;
+    
+    displayQuestion();
+})
